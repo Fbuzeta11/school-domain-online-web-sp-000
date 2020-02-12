294e230a1bf74d663488b1a89946c9e4622f09c7
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-02-12 01:42:49 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,6 @@
-school = School.new("Bayside High School")# code here!
+school = School.new("Bayside High School")
+
+school.roster
+
+school.add_student("Zach Morris", 9)
+school.roster # code here!
