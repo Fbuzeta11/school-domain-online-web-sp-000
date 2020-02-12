5ef1f526849acb4399b03dc62633ce4656cbe447
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-02-12 02:03:20 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -30,20 +30,7 @@ class School
 end 
 end
 
-school = School.new("Bayside High School")
 
-school.roster
-
-school.add_student("Zach Morris", 9)
-school.roster # code here!
-
-hash["new_key"] = []
-hash["new_key"] << "new_value_for_value_array"
-
-school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
 
 school.grade(9)
 
